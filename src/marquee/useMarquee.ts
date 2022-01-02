0dd4cd45b87c09c8185b1ce723aeba6454a0d381
limichange <limichange@hotmail.com>
2022-01-02 12:40:07 +0800

    feat: useMarquee base

--- src/marquee/useMarquee.ts
+++ src/marquee/useMarquee.ts
@@ -1,12 +1,35 @@
+import { useEffect, useRef, useState } from 'react'
+
 interface UseMarqueeOptions {
   speed?: number
 }
 
 export function useMarquee(options: UseMarqueeOptions = {}) {
+  const ref = useRef<HTMLDivElement>(null)
   const { speed = 10 ** 4 } = options
+  const [containerWidth, setContainerWidth] = useState(0)
+  const [keyframes, setKeyframes] = useState<{ transform: string }[]>([])
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) return
+
+    const keyframes = [
+      { transform: 'translateX(0)' },
+      { transform: 'translateX(-100%)' },
+    ]
+
+    const { width: containerWidth } = element.getBoundingClientRect()
+
+    setContainerWidth(containerWidth)
+    setKeyframes(keyframes)
+  }, [])
 
   return {
     marqueeProps: {
+      keyframes,
+      containerWidth,
+      ref,
       speed,
     },
   }
